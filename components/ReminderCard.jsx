02ac1446f99c70cddac62dc0f159c230a1f81eed
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-04-26 12:38:52 +0600

    done ui implementatio

--- components/ReminderCard.jsx
+++ components/ReminderCard.jsx
@@ -1,14 +1,27 @@
+import { toast } from "react-hot-toast";
+
 const ReminderCard = ({ item }) => {
   const { title, date } = item;
   const my_date = date.slice(0, 10);
   const my_time = date.slice(11, 16);
+  const handleDelete = () => {
+    fetch(`https://reminder-server.vercel.app/reminder/${item._id}`, {
+      method: "DELETE",
+    }).then(() => {
+      toast.remove("Reminder deleted");
+      window.location.reload();
+    });
+  };
 
   return (
     <div className="shadow rounded-xl">
       <div className="px-4 py-2">
         <div className="flex justify-between">
           <h1 className="text-xl font-bold ">{title}</h1>
-          <button className="bg-red-400 text-white px-2 rounded-md">
+          <button
+            onClick={handleDelete}
+            className="bg-red-400 text-white px-2 rounded-md"
+          >
             delete
           </button>
         </div>
